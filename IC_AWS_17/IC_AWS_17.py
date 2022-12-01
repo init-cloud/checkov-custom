949d83159ebbce7d8fccb84d5c64f5457b8f03bf
pj991207 <pj991207@gmail.com>
2022-12-01 12:55:38 +0900

    [22.12.01][add]IC_AWS_16~20

---
+++ IC_AWS_17/IC_AWS_17.py
@@ -0,0 +1,25 @@
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+
+
+class IC_AWS_18(BaseResourceCheck):
+    bucket_name = list()
+
+    def __init__(self) -> None:
+        name = "Do not declare the same bucket name repeatedly."
+        id = "IC_AWS_18"
+        supported_resources = ("aws_s3_bucket_acl",)
+        categories = (CheckCategories.GENERAL_SECURITY,)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        if 'bucket' in conf.keys():
+            bucket = conf['bucket'][0]
+            if bucket in self.bucket_name:
+                return CheckResult.FAILED
+
+            self.bucket_name.append(bucket)
+            return CheckResult.PASSED
+
+
+check = IC_AWS_18()
